ee419f2f9c52dfde7e3b28a3ac71eca14278028c
Shauna <sboddwell2@gmail.com>
2018-08-04 13:43:57 -0400

    marc is trying to help and might be failing.  But maybe not.  It's gonna be great.

---
+++ assets/javascript/youTubeAPI.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    
+    $(document).on("click", '.track', function (event) {
+        var track = $(this).attr('data-track');
+        youtubeApiCall(track);
+        console.log("ON CLICK");
+        return false;
+    });
+    function youtubeApiCall(track) {
+        var artist = $("#artistName").val();
+        var track = track;
+        var query = artist + " " + track;
+        console.log(query);
+        $.ajax({
+            cache: false,
+            data: $.extend({
+                key: 'AIzaSyBd9qzoC2CKQXsDv--fsMVj7BqOV_285ys',
+                q: query,
+                part: 'snippet',
+            }, { maxResults: 1, type: 'video', videoEmbeddable: 'true' }),
+            dataType: 'json',
+            type: 'GET',
+            timeout: 5000,
+            url: 'https://www.googleapis.com/youtube/v3/search'
+        })
+            .done(function (data) {
+                var video = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + data.items[0].id.videoId + '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+                $("#youtubeBox").append(video);
+                console.log(data);
+            });
+    }
+});
